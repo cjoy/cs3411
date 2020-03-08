26a876f2905b6691510aa201f5b1f9ff06e90f5e
Chris Joy <chrisjoyofficial@gmail.com>
2020-03-08 17:17:46 +1100

    Fixed ex1

--- ass1/assign1_part1.pl
+++ ass1/assign1_part1.pl
@@ -3,7 +3,7 @@ sumsq_even([], 0).
 sumsq_even([H | T], Sum) :- 
     mod(H, 2) =:= 0,
     sumsq_even(T, Rest),
-    Sum is H + Rest.
+    Sum is H*H + Rest.
 sumsq_even([H | T], Sum) :-
     mod(H, 2) \== 0,
     sumsq_even(T, Sum).
