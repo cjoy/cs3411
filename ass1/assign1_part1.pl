7b2f8318a4d995e2a201f67488d5a3885626f877
Chris Joy <chrisjoyofficial@gmail.com>
2020-03-10 23:10:51 +1100

    Incremental progress on assignment 1

--- ass1/assign1_part1.pl
+++ ass1/assign1_part1.pl
@@ -1,4 +1,7 @@
+% Assignment 1: Prolog and Search | COMP3411 20T1
+% Chris Joy, z5113243
 
+% Q1.1: sumsq_even(Numbers, Sum) - sums the squares of only the even numbers in a list of integers
 sumsq_even([], 0).
 sumsq_even([H | T], Sum) :- 
     mod(H, 2) =:= 0,
@@ -8,5 +11,23 @@ sumsq_even([H | T], Sum) :-
     mod(H, 2) \== 0,
     sumsq_even(T, Sum).
 
+% Q1.2: log_table(NumberList, ResultList) - that binds ResultList to the list of pairs consisting of a number and its log, for each number in NumberList
+log_table([], []).
+log_table([H | T], ResultList) :-
+    log_table(T, Rest),
+    X is log(H),
+    append([[H, X]], Rest, ResultList).
 
-% sumsq_even([1,3,5,2,-4,6,8,-7], Sum).
+% Q1.3: paruns(List,RunList) - converts a list of numbers into the corresponding list of parity runs
+% paruns([],[]).
+% paruns([A,B|T], [[A,B]]) :- 
+%     mod(A, 2) =:= 0,
+%     mod(B, 2) =:= 0.
+
+
+% Q1.4: eval(Expr, Val) - that will evaluate an expression
+eval(A, Val) :- number(A), Val is A.
+eval(add(A, B), Val) :- eval(A, X), eval(B, Y), Val is X + Y.
+eval(sub(A, B), Val) :- eval(A, X), eval(B, Y), Val is X - Y.
+eval(mul(A, B), Val) :- eval(A, X), eval(B, Y), Val is X * Y.
+eval(div(A, B), Val) :- eval(A, X), eval(B, Y), Val is X / Y.
---
+++ ass1/tmp.py
@@ -0,0 +1,20 @@
+def parun(arr, res):
+  if len(arr) == 0:
+    return []
+  
+  if len(res) == 0:
+    res.append([arr[0]])
+  elif arr[0] % 2 == 0 and res[-1][-1] % 2 == 0:
+    res[-1].append(arr[0])
+  elif arr[0] % 2 != 0 and res[-1][-1] % 2 != 0:
+    res[-1].append(arr[0])
+  elif arr[0] % 2 == 0 and res[-1][-1] % 2 != 0:
+    res.append([arr[0]])
+  elif arr[0] % 2 != 0 and res[-1][-1] % 2 == 0:
+    res.append([arr[0]])
+
+  return parun(arr[1:], res)
+
+par = []
+parun([8,0,4,3,7,2,-1,9,9], par)
+print(par)
