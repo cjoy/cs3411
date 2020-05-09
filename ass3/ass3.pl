410607331d215309d86f266e6f789f57ff02f6c4
Chris Joy <chrisjoyofficial@gmail.com>
2020-05-09 12:44:14 +1000

    Finish off last assignment

--- ass3/ass3.pl
+++ ass3/ass3.pl
@@ -176,21 +176,21 @@ process(event(Event, EventL), Ref1, Ref2) :-
 
 % process events with conjuctions
 process(event(Event, EventL), Ref1, Ref2) :-
-	EventL = [actor(set(thing(Entity1,_),thing(Entity2,__))), object(Object)],
-	process(set(thing(Entity1,_),thing(Entity2,__)), Ref1, Ref3),
+	EventL = [actor(set(thing(Entity1,Attr1),thing(Entity2,Attr2))), object(Object)],
+	process(set(thing(Entity1,Attr1),thing(Entity2,Attr2)), Ref1, Ref3),
 	process(Object, Ref3, Ref2),
     assert(history(event(Event, EventL))).
 
 % Append sets to history
-process(set(thing(Entity1, Attr1),thing(Entity2,Attr2)), Ref1, Ref3) :-
+process(set(thing(Entity1, Attr1),thing(Entity2,Attr2)), _Ref1, _Ref3) :-
 	assert(history(set(thing(Entity1,Attr1),thing(Entity2,Attr2)))).
 
 % Looks for set in history
 process(personal(they), Ref1, Ref2) :-
-	history(set(thing(Entity1, __), thing(Entity2, __))),
+	history(set(thing(Entity1, _Attr1), thing(Entity2, _Attr2))),
 	merge_list(Ref1, [[Entity1, Entity2]], Ref2).
 process(personal(those), Ref1, Ref2) :-
-	history(set(thing(Entity1, __), thing(Entity2, __))),
+	history(set(thing(Entity1, _Attr1), thing(Entity2, _Attr2))),
 	merge_list(Ref1, [[Entity1, Entity2]], Ref2).
 
 
@@ -225,37 +225,3 @@ process(personal(Pronoun), Ref1, Ref2) :-
 	personal(Pronoun, [number(N), gender(G)]),
 	history(thing(Name, [isa(physical_object), gender(G), number(N)])),
 	merge_list(Ref1, [Name], Ref2).
-
-
-
-
-% Test cases from lecturer:
-% Base cases that you should be able to handle
-% run([john,lost,his,wallet], Refs).
-% Refs = [John].
-% run([john,lost,her,wallet], Refs).
-% false.
-% run([mary,lost,her,wallet], Refs).
-% Refs = [mary].
-% run([she,looked,for,it], Refs).
-% Refs = [mary, wallet].
-
-% abolish(history/1)
-% run([mary,lost,her,wallet], Refs).
-% Refs = [mary].
-% run([he,looked,for,it], Refs).
-% false.
-% run([john,looked,for,it], Refs).
-% Refs = [wallet].
-% run([he,found,it], Refs).
-% Refs = [john, wallet].
-
-% abolish(history/1).
-% run([mary,gave,the wallet,to,john], Refs).
-% Refs = [].
-% run([he,lost,it], Refs).
-% Refs = [john, wallet].
-% run([john,and,mary,looked,for,it], X).
-% Refs = [wallet].
-% run([they,found,it], Refs).
-% Refs = [[john, mary], wallet].
